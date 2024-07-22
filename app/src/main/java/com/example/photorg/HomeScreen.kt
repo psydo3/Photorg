c4a4b742afea9159edf14ef400e01551c137b68e
Cengiz <dzhengizosman1@gmail.com>
2024-07-22 19:27:24 +0100

    added album images and new color options

--- app/src/main/java/com/example/photorg/HomeScreen.kt
+++ app/src/main/java/com/example/photorg/HomeScreen.kt
@@ -1,19 +1,28 @@
 package com.example.photorg
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,11 +31,13 @@ import androidx.compose.ui.unit.sp
 fun HomeScreen() {
     Column {
         TopBar()
+        Spacer(modifier = Modifier.height(16.dp))
+        AlbumSection()
     }
 }
 
 @Composable
-fun TopBar(modifier: Modifier = Modifier) {
+fun TopBar() {
     Row (
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -41,15 +52,35 @@ fun TopBar(modifier: Modifier = Modifier) {
             fontSize = 24.sp
         )
 
-        //change icon to real image
-        Icon(
-            imageVector = Icons.Default.Create,
+        Image(
+            painter = painterResource(R.drawable.album),
             contentDescription = null,
             modifier = Modifier
-                .size(30.dp)
+                .size(40.dp)
                 .clickable {
                     //implement later
                 }
+        )
+    }
+}
+
+@Composable
+fun AlbumSection() {
+    Album()
+}
+
+@Composable
+fun Album(
+    albumName: String? = "Unnamed Album",
+    albumColor: Color = colorResource(R.color.pink),
+) {
+    Box(
+
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.album_icon),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(albumColor)
         )
     }
 }
